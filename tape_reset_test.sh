ba71d68d4c7794008d1fe9bfb5791e0298b25983
John Meneghini <jmeneghi@redhat.com>
2025-10-28 11:33:05 -0400

    fix sleep times in tape_reset_test.sh
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- tape_reset_test.sh
+++ tape_reset_test.sh
@@ -35,7 +35,7 @@ set_debug
 set_dmesg
 
 echo ""
-lsscsi -ig
+lsscsi -igN
 echo ""
 
 TDEV=$(echo "$DEV" | awk -F"/" '{print $3}')
@@ -87,10 +87,12 @@ do_cmd_true "mt -f $DEV status"
 test_reset_blocked_false "$TDEV"
 
 #
-# Reset the device
+# Reset the device and wait
 #
 $DIR/tape_reset.sh $SDEV 5 &
-sleep 3
+
+echo "Sleep for 10 seconds"
+sleep 10
 
 do_cmd_true "sg_map -st -x -i"
 test_reset_blocked_true "$TDEV"
@@ -138,7 +140,7 @@ do_cmd_true "sg_map -st -x -i"
 test_reset_blocked_false "$TDEV"
 
 #
-# Reset the device
+# Reset the device with IO in progress
 #
 $DIR/tape_reset.sh $SDEV 5 &
 
@@ -149,15 +151,16 @@ test_reset_blocked_false "$TDEV"
 do_cmd_false "dd if=/dev/random count=1001024 of=$DEV"
 test_reset_blocked_true "$TDEV"
 
-#
-# Seek should succeed after reset
-#
 do_cmd_true "sg_map -st -x -i"
 test_reset_blocked_true "$TDEV"
 do_cmd_true "stinit -f $DIR/stinit.conf -v $DEV"
 test_reset_blocked_true "$TDEV"
 do_cmd_true "mt -f $DEV status"
 test_reset_blocked_true "$TDEV"
+
+#
+# Seek should succeed after reset
+#
 do_cmd_true "mt -f $DEV eod"
 test_reset_blocked_false "$TDEV"
 do_cmd_true "mt -f $DEV status"
@@ -193,6 +196,9 @@ test_reset_blocked_false "$TDEV"
 #
 $DIR/tape_reset.sh $SDEV 5 &
 
+echo "Sleep for 10 seconds"
+sleep 10
+
 #
 # This command should fail
 #
@@ -213,6 +219,8 @@ test_reset_blocked_false "$TDEV"
 # Reset the device with no tape
 #
 $DIR/tape_reset.sh $SDEV 1 &
+
+echo "Sleep for 3 seconds"
 sleep 3
 
 #
@@ -222,22 +230,24 @@ do_cmd_true "mt -f $DEV status"
 test_reset_blocked_true "$TDEV"
 
 #
-# These command fail when there's no tape.
+# These commands fail when there's no tape.
 #
 do_cmd_false "mt -f $DEV weof 1"
 do_cmd_false "mt -f $DEV wset 1"
 test_reset_blocked_true "$TDEV"
 do_cmd_false "mt -f $DEV eod"
-test_reset_blocked_false "$TDEV"
+test_reset_blocked_true"$TDEV"
 do_cmd_false "dd if=$DEV count=1024 of=/dev/null"
 do_cmd_false "dd if=/dev/random count=1001024 of=$DEV"
-test_reset_blocked_false "$TDEV"
+test_reset_blocked_true "$TDEV"
 
 #
 # Load the tape should succed
 #
 do_cmd_true "mt -f $DEV status"
+test_reset_blocked_true "$TDEV"
 do_cmd_true "mt -f $DEV load"
+test_reset_blocked_false "$TDEV"
 do_cmd_true "mt -f $DEV status"
 do_cmd_true "mt -f $DEV eod"
 do_cmd_true "mt -f $DEV status"
@@ -247,6 +257,8 @@ test_reset_blocked_false "$TDEV"
 # Reset the device with tape at EOD
 #
 $DIR/tape_reset.sh $SDEV 1 &
+
+echo "Sleep for 3 seconds"
 sleep 3
 
 #
@@ -275,6 +287,8 @@ do_cmd_true "mt -f $DEV status"
 # Reset the device while at BOT
 #
 $DIR/tape_reset.sh $SDEV 1 &
+
+echo "Sleep for 3 seconds"
 sleep 3
 
 #
